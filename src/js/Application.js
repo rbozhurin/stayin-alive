77dcc8175f0a610358625f00b8ff95ada5c37e2f
Rayko Bozhurin <rbozhurin@gmail.com>
2023-01-16 21:14:33 +0200

    ssssssssssss

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,5 +1,5 @@
 import EventEmitter from "eventemitter3";
-import Beat from "./Beat.js";
+import Beat from "./Beat";
 
 export default class Application extends EventEmitter {
   static get events() {
@@ -11,21 +11,19 @@ export default class Application extends EventEmitter {
   constructor() {
     super();
     this._beat = new Beat();
-    this._create();
-    this.emit(Application.events.READY);
-    const lyrics = ["Ah", "ha", "ha", "ha", "stain' alive", "stain' alive"];
+    const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
     let count = 0;
 
     this._beat.addListener(Beat.events.BIT, () =>
       this._create(lyrics[count > lyrics.length - 1 ? (count = 0) : count++])
     );
+    this.emit(Application.events.READY);
   }
 
   _create(msg) {
     const message = document.createElement("div");
     message.classList.add("message");
     message.innerText = msg;
-
     document.querySelector(".main").appendChild(message);
   }
 }
