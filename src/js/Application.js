3e864786821794f89f9973c1d4d471d9f3e5a943
Rayko Bozhurin <rbozhurin@gmail.com>
2023-01-16 20:42:43 +0200

    asasa

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,4 +1,5 @@
 import EventEmitter from "eventemitter3";
+import Beat from "./Beat.js";
 
 export default class Application extends EventEmitter {
   static get events() {
@@ -9,12 +10,26 @@ export default class Application extends EventEmitter {
 
   constructor() {
     super();
-
-    const button = document.querySelector(".button");
-    button.addEventListener("click", () => {
-      alert("💣");
-    });
-
+    this._beat = new Beat();
+    this._create();
     this.emit(Application.events.READY);
+  }
+
+  _create() {
+    const lyrics = ["Ah", "ha", "ha", "ha", "stain' alive", "stain' alive"];
+    let count = 0;
+
+    this._beat.on(Beat.events.BIT, (bit) => {
+      const message = document.createElement("div");
+      message.classList.add("message");
+      message.innerText = lyrics[count];
+      count++;
+
+      if (count == 6) {
+        count = 0;
+      }
+
+      document.querySelector(".main").appendChild(message);
+    });
   }
 }
